435ee58d3e24cede66c04475db5d8bd6fcddb0d3
Raissa Arantes <rayssa.hcl@gmail.com>
2020-10-27 01:09:27 -0200

    Adiçao e novas telas, Crianção de Interface básica

---
+++ Musicfy/app/src/main/java/br/com/musicfy/AddMusic.kt
@@ -0,0 +1,16 @@
+package br.com.musicfy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class AddMusic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_music)
+    }
+
+    fun irSalvarMusica(component: View){
+
+    }
+}
--- Musicfy/app/src/main/java/br/com/musicfy/MainActivity.kt
+++ Musicfy/app/src/main/java/br/com/musicfy/MainActivity.kt
@@ -1,9 +1,9 @@
 package br.com.musicfy
 
-import android.graphics.Color
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.TypedValue
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import br.com.musicfy.http.MusicApi
@@ -19,46 +19,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val novoTv = TextView(baseContext)
-
-
-        ll_conteudo.addView(novoTv)
-
-        consumirApiMusicas()
-    }
-
-
-    fun consumirApiMusicas() {
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://5f907b7ee0559c0016ad69ee.mockapi.io/musicfy-api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        //É criada a implementação em tempo real da Api
-        val requests = retrofit.create(MusicApi::class.java)
-
-
-
-        //getFilmes é o endpoits que retorna uma call --preparação da chamada da api
-        val callMusic = requests.getMusics()
-
-        callMusic.enqueue(object: Callback<List<Music>> {
-            override fun onFailure(call: Call<List<Music>>, t: Throwable) {
-                Toast.makeText(applicationContext, "Deu ruim $t", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onResponse(call: Call<List<Music>>, response: Response<List<Music>>) {
-                response.body()?.forEach {
-                    val novoTv = TextView(baseContext)
-
-                    novoTv.text = "${it.id} - ${it.nome} - ${it.artista} -${it.album}"
-                    novoTv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
-                    novoTv.setTextColor(Color.parseColor("#000000"))
-
-                    ll_conteudo.addView(novoTv)
-                }
-            }
-
-        })
 
     }
+
+
+    fun irAdicionarMusica(component:View){
+        val addMusicaActivity = Intent(this@MainActivity, AddMusic::class.java)
+        startActivity(addMusicaActivity)
+    }
+
+    fun irBuscarMusicas(component:View){
+        val myMusicListActivity = Intent(this@MainActivity, MyMusicList::class.java)
+        startActivity(myMusicListActivity)
+    }
+
 }
